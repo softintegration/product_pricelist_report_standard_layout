a2591b32ab7f9f0d6558d81c101c97780b953a09
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-12-06 21:44:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Pricelist report standard layout
+--------------------------------
+This module replace the non formated layout of the Pricelist report by the standard layout used by all the reports. 
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Pricelist report standard layout',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Product',
+    'demo': [],
+    'depends': ['product'],
+    'data': [
+        'report/product_pricelist_report_templates.xml',
+    ],
+    'license': 'LGPL-3',
+}
